6a9102a75ccf241eafb3356ebff8c1bd4b498303
noamkux <noam.kux@gmail.com>
2023-05-15 20:21:29 +0300

    last changes

--- js/contact.js
+++ js/contact.js
@@ -6,11 +6,7 @@ form.addEventListener('submit', (e) => {
   const email = form.email.value.trim();
   const message = form.message.value.trim();
 
-  // Here you can add your logic to handle the form submission, such as sending an email or storing the message in a database
-
-  // For now, let's just log the form data to the console
   console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
 
-  // Clear the form after submission
   form.reset();
 });
--- js/navBarToglle.js
+++
@@ -1,8 +0,0 @@
-/* Add JavaScript to make the menu header component interactive */
-
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarNav = document.querySelector('.navbar-nav');
-
-navbarToggler.addEventListener('click', function() {
-  navbarNav.classList.toggle('show');
-});
